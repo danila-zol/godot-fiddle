9d2ff3088e6b543085a564a1a0216d6e77162399
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2024-12-05 19:50:24 +0300

    Fixed syntax errors in the migration init file

--- backend/database/data/001_init.sql
+++ backend/database/data/001_init.sql
@@ -1,19 +1,6 @@
-CREATE TABLE "demos" (
-  "id" integer  PRIMARY KEY,
-  "name" varchar(255) NOT NULL,
-  "description" text NOT NULL,
-  "user_id" integer NOT NULL,
-  "link" varchar(255) NOT NULL,
-  "created_at" timestamp NOT NULL,
-  "updated_at" timestamp NOT NULL,
-  "upvotes" integer NOT NULL,
-  "downvotes" integer NOT NULL,
-  "topic_id" integer NOT NULL
-);
-
-CREATE TABLE "demo_access" (
-  "demo_id" integer NOT NULL,
-  "user_id" integer NOT NULL
+CREATE TABLE "roles" (
+  "id" integer PRIMARY KEY,
+  "name" varchar(255) NOT NULL
 );
 
 CREATE TABLE "users" (
@@ -22,14 +9,9 @@ CREATE TABLE "users" (
   "display_name" varchar(255),
   "email" varchar(255) NOT NULL UNIQUE,
   "password" varchar(255) NOT NULL,
-  "role_id" integer NOT NULL,
+  "role_id" integer NOT NULL REFERENCES "roles" ("id"),
   "created_at" timestamp NOT NULL,
   "karma" integer NOT NULL
-);
-
-CREATE TABLE "roles" (
-  "id" integer PRIMARY KEY,
-  "name" varchar(255) NOT NULL
 );
 
 CREATE TABLE "forums" (
@@ -39,51 +21,51 @@ CREATE TABLE "forums" (
 
 CREATE TABLE "topics" (
   "id" integer PRIMARY KEY,
-  "title" varchar(255), NOT NULL
-  "user_id" integer NOT NULL,
-  "forum_id" integer NOT NULL,
+  "title" varchar(255) NOT NULL,
+  "user_id" integer NOT NULL REFERENCES "users" ("id"),
+  "forum_id" integer NOT NULL REFERENCES "forums" ("id"),
   "tag" varchar(255) NOT NULL,
   "created_at" timestamp NOT NULL,
   "last_update" timestamp NOT NULL,
-  "total_upvotes" integer NOT NULL
+  "total_upvotes" integer NOT NULL,
   "total_downvotes" integer NOT NULL
+);
+
+CREATE TABLE "demos" (
+  "id" integer  PRIMARY KEY,
+  "name" varchar(255) NOT NULL,
+  "description" varchar NOT NULL,
+  "user_id" integer NOT NULL REFERENCES "users" ("id"),
+  "link" varchar(255) NOT NULL,
+  "created_at" timestamp NOT NULL,
+  "updated_at" timestamp NOT NULL,
+  "upvotes" integer NOT NULL,
+  "downvotes" integer NOT NULL,
+  "topic_id" integer NOT NULL REFERENCES "topics" ("id")
+);
+
+CREATE TABLE "demo_access" (
+  "demo_id" integer NOT NULL REFERENCES "demos" ("id"),
+  "user_id" integer NOT NULL REFERENCES "users" ("id")
 );
 
 CREATE TABLE "messages" (
   "id" integer PRIMARY KEY,
-  "topic_id" integer NOT NULL,
-  "user_id" integer NOT NULL,
+  "topic_id" integer NOT NULL REFERENCES "topics" ("id"),
+  "user_id" integer NOT NULL REFERENCES "users" ("id"),
   "title" varchar(255) NOT NULL,
-  "body" text NOT NULL,
-  "upvotes" integer NOT NULL
+  "body" varchar NOT NULL,
+  "upvotes" integer NOT NULL,
   "downvotes" integer NOT NULL
 );
 
 CREATE TABLE "assets" (
   "id" integer PRIMARY KEY,
   "name" varchar(255) NOT NULL,
-  "description" text NOT NULL,
+  "description" varchar NOT NULL,
   "link" varchar(255) NOT NULL,
   "created_at" timestamp NOT NULL
 );
-
-ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "demos" ("user_id");
-
-ALTER TABLE "topics" ADD FOREIGN KEY ("id") REFERENCES "demos" ("topic_id");
-
-ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "demo_access" ("user_id");
-
-ALTER TABLE "demos" ADD FOREIGN KEY ("id") REFERENCES "demo_access" ("demo_id");
-
-ALTER TABLE "roles" ADD FOREIGN KEY ("id") REFERENCES "users" ("role_id");
-
-ALTER TABLE "topics" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
-ALTER TABLE "topics" ADD FOREIGN KEY ("forum_id") REFERENCES "forums" ("id");
-
-ALTER TABLE "messages" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
-ALTER TABLE "messages" ADD FOREIGN KEY ("topic_id") REFERENCES "topics" ("id");
 
 ---- create above / drop below ----
 
