47b8236ae17cfaad4830c64900110078fce590df
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2025-04-15 23:15:09 +0300

    Fixed typo in "omitempty" tag

--- backend/internal/domain/models/user.go
+++ backend/internal/domain/models/user.go
@@ -6,31 +6,31 @@ import (
 )
 
 type Session struct {
-	ID     *string `json:"id,omitempy"`
-	UserID *string `json:"userID,omitempy"`
+	ID      *string `json:"id,omitempty"`
+	UserID  *string `json:"userID,omitempty"`
 }
 
 type LoginForm struct {
-	Username *string `json:"username,omitempy"`
-	Email    *string `json:"email,omitempy"`
+	Username *string `json:"username,omitempty"`
+	Email    *string `json:"email,omitempty"`
 	Password *string `json:"password"`
 }
 
 type Role struct {
-	ID   *string `json:"id,omitempy"`
-	Name *string `json:"name,omitempy"`
+	ID   *string `json:"id,omitempty"`
+	Name *string `json:"name,omitempty"`
 	// Permissions []gorbac.Permission `json:"permissions"`
 	//TODO: How do we manage permissions?
 }
 
 type User struct {
-	ID          *string    `json:"id,omitempy"`
-	Username    *string    `json:"username,omitempy"`
-	DisplayName *string    `json:"displayName,omitempy"`
-	Email       *string    `json:"email,omitempy"`
+	ID          *string    `json:"id,omitempty"`
+	Username    *string    `json:"username,omitempty"`
+	DisplayName *string    `json:"displayName,omitempty"`
+	Email       *string    `json:"email,omitempty"`
 	Password    *string    `json:"password"`
 	Verified    *bool      `json:"verified,omitempty"` // TODO: Verification endpoint
-	RoleID      *string    `json:"roleID,omitempy"`
+	RoleID      *string    `json:"roleID,omitempty"`
 	CreatedAt   *time.Time `json:"createdAt,omitzero"`
-	Karma       *int       `json:"karma,omitempy"`
+	Karma       *int       `json:"karma,omitempty"`
 }
