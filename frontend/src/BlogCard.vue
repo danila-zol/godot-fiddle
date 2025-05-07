e5994f51f70e9ef19c0bdfefc7d9d2644787738d
Danila <danila-zol@list.ru>
2025-05-07 12:53:50 +0300

    Start frontend work

---
+++ frontend/src/BlogCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps(['post'])
+</script>
+
+<template>
+    <div class="blog-card">
+        <a :href='"/" + post.title'>
+            <img class="blog-thumbnail" :src="post.thumb">
+            <p id="blog-title">{{ post.title }}</p>
+            <p id="blog-description">{{ post.text }}</p>
+        </a>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.blog-card {
+    display: flex;
+    flex-direction: column;
+}
+
+.blog-card a {
+    text-decoration: none;
+    color: black;
+    border-style: solid;
+}
+
+.blog-thumbnail {
+    max-height: 300px;
+    max-width: 300px;
+}
+</style>
---
+++ frontend/src/Blogposts.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { blogs } from './globals';
+import BlogCard from './BlogCard.vue';
+</script>
+
+<template>
+<div id="blogs">
+    <BlogCard v-for="blog in blogs" :post="blog"></BlogCard>
+</div>
+</template>
+
+<style lang="css" scoped>
+#blogs {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
