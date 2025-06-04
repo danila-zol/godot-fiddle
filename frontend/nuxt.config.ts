384aaed34ff84b40810f05571247e0bcf176e73d
Danila <danila-zol@list.ru>
2025-06-04 04:06:23 +0300

    Optimize font loading

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   css: ['~/assets/css/main.scss'],
+
   runtimeConfig: {
     public: {
       docsUrl: "/docs",
@@ -12,12 +13,31 @@ export default defineNuxtConfig({
       enginePath: "/thirdparty/godot/"
     }
   },
-  routeRules : {
-    '/**' : {cors: true, headers: { 
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp"
-    }},
-    '/new' : {ssr: false},
-    '/games/*': {ssr: false}
+
+  routeRules: {
+    '/**': {
+      headers: {
+        "Cross-Origin-Opener-Policy": "same-origin",
+        "Cross-Origin-Embedder-Policy": "require-corp"
+      }
+    },
+    '/new': { ssr: false },
+    '/games/*': { ssr: false }
   },
+
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+      ],
+      htmlAttrs: {
+        lang: "ru"
+      },
+      title: "ИгроЦех - сервер игр на Godot",
+      meta: [
+        { name: "description", content: "ИгроЦех - сервер игр на Godot" }
+      ]
+    }
+  }
 })
